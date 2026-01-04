2bf7d85a91a6c8ad7ee0e817cc30bf015c81fcb2
Devanshu Jangid <156595156+Devanshujangid@users.noreply.github.com>
2026-01-04 23:16:47 +0530

    Time: 0 ms (100%), Space: 59.5 MB (7.99%) - LeetHub

--- 0701-insert-into-a-binary-search-tree/0701-insert-into-a-binary-search-tree.cpp
+++ 0701-insert-into-a-binary-search-tree/0701-insert-into-a-binary-search-tree.cpp
@@ -12,20 +12,38 @@
 class Solution {
 public:
     TreeNode* insertIntoBST(TreeNode* root, int val) {
-        // will try to solve this question via recurssion
-
-       //base case
-       if ( root==NULL ){
-          // insertion happens
-          return new TreeNode(val);
-       }
-        
-        else if ( root->val < val ){
-            // move to right
-           root->right= insertIntoBST(root->right,val);
-        }
-        else {
-            root->left=insertIntoBST(root->left,val);
+        if ( root==NULL ) return new TreeNode(val);
+        // create a copy of this node
+        TreeNode* curr=root;
+        //keep traversing
+        while (true){
+            // if current value < value to be inserted -->> move to right
+            if ( curr->val < val ){
+                // if the place at right is not null--> keep moving right
+                if ( curr->right != NULL ){
+                    curr = curr->right;
+                }
+                else{
+                    // insert it here-->insertion should be on right side
+                    curr->right = new TreeNode(val);  // for insertion make a new node
+                    // as soon as you inserted 
+                    break;
+                }
+            }
+            // if current value > value to be inserted -->> move to left
+            else {
+                // if the place at left is not null--> keep moving left
+                if ( curr->left != NULL ) {
+                    curr = curr->left;
+                }
+                else{
+                    // insert it here in left
+                    curr->left= new TreeNode(val);
+                    // as soon as you inserted 
+                    break;
+                }
+            }
+            
         }
         return root;
     }
