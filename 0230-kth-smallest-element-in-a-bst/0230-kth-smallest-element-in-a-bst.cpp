a9fb9b0e12c4296ce2b1d0d4a9a9c6f664ae7b83
Devanshu Jangid <156595156+Devanshujangid@users.noreply.github.com>
2026-01-07 01:13:34 +0530

    Time: 0 ms (100%), Space: 24.5 MB (41.15%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
@@ -11,16 +11,26 @@
  */
 class Solution {
 public:
+    int ans=-1; // this is what we will return 
+    int seen_count=0; // how many times you seen that root?
+    
     int kthSmallest(TreeNode* root, int k) {
-        vector<int>temp;
-        vector<int>ans=inorder(root,temp);
-        return ans[k-1];
+        inorder(root,k);
+        return ans;
     }
-    vector<int>inorder(TreeNode* root, vector<int>& temp){
-        if ( root==NULL ) return temp;
-        inorder(root->left,temp);
-        temp.push_back(root->val);
-        inorder(root->right,temp);
-        return temp;
+
+    void inorder(TreeNode* root, int k){
+        // base case
+        if ( !root || ans!=-1 ) return;
+
+        inorder(root->left,k);
+        if ( ans!=-1) return; // prevent right traversal
+        seen_count++;
+        // when seen_count==k return that node
+        if ( seen_count==k ) {
+            ans=root->val;
+            return;
+        }
+        inorder(root->right,k);
     }
 };
