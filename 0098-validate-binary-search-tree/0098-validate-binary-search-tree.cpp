968d7a495c053d460f6468a8e5c6ebe42cd543d8
Devanshu Jangid <156595156+Devanshujangid@users.noreply.github.com>
2026-01-07 23:50:13 +0530

    Time: 0 ms (100%), Space: 21.8 MB (74.12%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -1,15 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    bool isValidBST(TreeNode* root, long long minAllowed, long long maxAllowed) {
-        if (root == NULL) return true;
+    bool isValidBST(TreeNode* root, long long min_Allowed , long long max_Allowed) {
+        // keep in mind
+        // Follow the Global BST rule->
+       //  1. Each node->val > all values in left subtree
+       //  2. Each node->val < all values in right subtree
+       
+       // for every node we will maintain a min_Allowed and max_Allowed
+       // such that -> min.Allowed < node->val < max.Allowed
+       
+       // base case 
+       if (root==NULL) return true;
 
-        if (root->val <= minAllowed || root->val >= maxAllowed)
-            return false;
-
-        return isValidBST(root->left, minAllowed, root->val) &&
-               isValidBST(root->right, root->val, maxAllowed);
+       // check  for the root value first
+       if ( min_Allowed >= root->val || max_Allowed <= root->val  ) return false;
+       return isValidBST(root->left, min_Allowed, root->val) &&
+               isValidBST(root->right, root->val, max_Allowed);
     }
-
     bool isValidBST(TreeNode* root) {
         return isValidBST(root, LLONG_MIN, LLONG_MAX);
     }
