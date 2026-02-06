18a148b950d5f18fa76406af298f2647848e65f9
Devanshu Jangid <156595156+Devanshujangid@users.noreply.github.com>
2026-02-06 23:12:59 +0530

    Time: 0 ms (100%), Space: 7.8 MB (95.93%) - LeetHub

---
+++ 1922-count-good-numbers/1922-count-good-numbers.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    static const long long MOD = 1e9 + 7;
+    long long pow(long long x , long long y ){ // we will use recurssion
+        // base case
+        if ( y==0 ) return 1;
+        long long temp = pow(x,y/2);
+        long long result = (temp * temp) % MOD;
+        // even
+        if ( y%2==0 ) return result;
+        else return (result*x)%MOD;
+    }
+    
+    int countGoodNumbers(long long n) {
+        // first of all calculate the number of odd and even positions
+        long long odd=n/2;
+        long long even=n/2+(n%2);
+        return (pow(5,even)*pow(4,odd))%MOD;
+    }
+};
