ce174d032a7e2a2ceaf51dc044a2a6021ad0f253
Devanshu Jangid <156595156+Devanshujangid@users.noreply.github.com>
2026-02-12 23:40:04 +0530

    Time: 0 ms (100%), Space: 13.4 MB (75.75%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,40 +1,33 @@
 class Solution {
 public:
+   // method 2 
     vector<string>result;
     vector<string> generateParenthesis(int n) {
         // via recurssion
         string curr="";
-        solve(curr,n);
+        int open=0;
+        int close=0;
+        solve(curr,n,open,close);
         return result; 
     }
 
-    void solve(string& curr , int n ){
+    void solve(string& curr , int n , int open , int close ){
         if ( curr.length() == 2*n ){  // base case where string formation stops 
-            // means now you have made string and now you have to store it in result but check it first
-            if (isValid(curr)){
-                result.push_back(curr);
-            }
+            result.push_back(curr);
             return;
         }
+        if ( open<n ){
         curr.push_back('('); // option 1->open bracket daal do
-        solve(curr,n);
+        solve(curr,n,open+1,close);
        // else put the close bracket
        curr.pop_back();
+        }
+        
+        if ( close<open ){
        curr.push_back(')'); // option 2->close bracket daal do 
        // again recurrsion to explore
-       solve(curr,n);
+       solve(curr,n,open,close+1);
        curr.pop_back();
-
-    }
-
-
-    bool isValid(string &str){
-        int count = 0;
-        for ( char &ch:str ){
-            if ( ch=='(' ) count++;
-            else count--; 
-            if ( count<0 )return false;
         }
-    return count==0;
     }
 };
