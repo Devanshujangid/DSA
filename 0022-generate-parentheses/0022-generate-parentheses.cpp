c6199c0635a61d197cdd3047da6fda0a76cc584f
Devanshu Jangid <156595156+Devanshujangid@users.noreply.github.com>
2026-02-12 23:28:18 +0530

    Time: 7 ms (11.18%), Space: 13.4 MB (73.46%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    vector<string>result;
+    vector<string> generateParenthesis(int n) {
+        // via recurssion
+        string curr="";
+        solve(curr,n);
+        return result; 
+    }
+
+    void solve(string& curr , int n ){
+        if ( curr.length() == 2*n ){
+            // means now you have made string and nnow you have to store it in result but check it first
+            if (isValid(curr)){
+                result.push_back(curr);
+            }
+            return;
+        }
+        curr.push_back('('); // option 1->open bracket daal do
+        solve(curr,n);
+       // else put the close bracket
+       curr.pop_back();
+       curr.push_back(')'); // option 2->close bracket daal do 
+       // again recurrsion to explore
+       solve(curr,n);
+       curr.pop_back();
+
+    }
+
+
+    bool isValid(string &str){
+        int count = 0;
+        for ( char &ch:str ){
+            if ( ch=='(' ) count++;
+            else count--; 
+            if ( count<0 )return false;
+        }
+    return count==0;
+    }
+};
