11e12a6d02e9eb619d7aea2721e651e1736f7fef
Devanshu Jangid <156595156+Devanshujangid@users.noreply.github.com>
2026-07-02 00:08:22 +0530

    Time: 14 ms (22.87%), Space: 7.8 MB (64.51%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,15 +1,11 @@
 class Solution {
 public:
     int fib(int n) {
-        // using DP
-
-        // initializing a dp array
-        vector<int>dp(n+1,-1); // size=n+1 & all elements initilized with -1
-        return helper(n,dp);
-    }
-    int helper(int n,vector<int> &dp){
-        if ( n<=1 ) return n;
-        if ( dp[n] != -1 ) return dp[n];
-        return dp[n]=helper(n-1,dp)+helper(n-2,dp);
+        // recurssion method!
+        
+        // base cond
+        if ( n==0  ) return 0;
+        if ( n==1 ) return 1;
+        return fib(n-1)+fib(n-2);
     }
 };
