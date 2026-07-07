c7d339be61073203a358948a23fbbd254c2a7fc5
Devanshu Jangid <156595156+Devanshujangid@users.noreply.github.com>
2026-07-08 00:57:27 +0530

    Time: 0 ms (100%), Space: 29.6 MB (68.92%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-        // optimal 
-        // reverse the array->reverese from 0-k and -> then reverse from k-n
-        int n = nums.size();
-        k = k%n;  // rotation repats after n
+        // tricky one:
+        // if k is larger than size of array - to prevent overflow
+        if (nums.size()>0)  k=k%nums.size();
+        // step 1: reverse of whole array
         reverse(nums.begin(),nums.end());
+        // step 2: reverse from 0 to k-1 && from k to n-1
         reverse(nums.begin(),nums.begin()+k);
-        reverse(nums.begin()+k,nums.end());
+        reverse(nums.begin()+k,nums.end()); 
     }
 };
