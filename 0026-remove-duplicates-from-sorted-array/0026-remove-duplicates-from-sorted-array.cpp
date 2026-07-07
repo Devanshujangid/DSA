dc66ca041e8f0bb162dd4ecd0fcbdd5506dc04af
Devanshu Jangid <156595156+Devanshujangid@users.noreply.github.com>
2026-07-07 23:47:56 +0530

    Time: 0 ms (100%), Space: 22.5 MB (98.15%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,26 +1,34 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        // using two pointers here
-        // i->let this will 'point to unique element' only
-        // j-> this wil search for unique element and give it to 'i'
         
-        // initially No. of unique elements=0
-        //int k=1;
-        int i=0;
-        for ( int j=1 ; j<nums.size() ; j++ ){
-           // if ( nums[i]==nums[j] ){
-                // so here no unique element
-                //j++;
-            //}
-            if ( nums[i] != nums[j] ){
-                // unique element is their!
-                //k++;
-                // i will make place for it
-                i=i+1;
-                nums[i]=nums[j];
+        // my first logic:
+        // use map -> but it will lead to O(n) extra space
+
+        // using 2 pointers 
+        // initializing pointers 
+        int ptr1=0;
+        int ptr2=1;
+        int n=nums.size();
+        // for the last inserted 
+        int lastInserted=-1;
+
+        while ( ptr1<n && ptr2<n ){
+            // case 1:
+            if ( nums[ptr1]==nums[ptr2] ){
+                ptr2++; // till nums[ptr1] != nums[ptr2] comes
+            } else {
+                // now here nums[ptr1] != nums[ptr2] comes
+                ptr1++;
+                if (lastInserted!=nums[ptr2]){
+                nums[ptr1] = nums[ptr2];
+                }
+                // update the last_inserted element
+                lastInserted=nums[ptr2];
             }
         }
-        return i+1;
+        // we need to return number of unique elements 
+        // unique elements vha tak rhenge jha tak ptr1 rhega.
+        return ptr1+1;
     }
 };
