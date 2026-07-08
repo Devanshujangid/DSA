9ae22c409b33f3c9aee29c985920f1b87db4a9cf
Devanshu Jangid <156595156+Devanshujangid@users.noreply.github.com>
2026-07-09 00:30:14 +0530

    Time: 0 ms (100%), Space: 23.8 MB (82.83%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        // placing the non zero element at the correct position
-        int place_idx=0;
-        for ( int i=0 ; i<nums.size() ; i++ ){
-            if ( nums[i]!=0 ){
-                swap(nums[place_idx],nums[i]);
-                place_idx++;
-            }
+        // we will find: the correct idx to place the non zero elements one by one.
+        int n=nums.size();
+        //traversing the array
+        int pnz=0;
+        for ( int i=0 ; i<n ; i++ ){
+                if (nums[i]!=0 ){
+                    swap(nums[pnz],nums[i]);
+                    pnz++;
+                }     
         }
     }
 };
