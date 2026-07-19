789413ed36f66e260a8f4f9deae9caee9346808c
Devanshu Jangid <156595156+Devanshujangid@users.noreply.github.com>
2026-07-19 23:51:48 +0530

    Time: 8 ms (38.35%), Space: 15.9 MB (5.29%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,27 +1,34 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        int n=nums.size();
-    
-        // decalre the map
-        unordered_map<int,int>mpp; // {key,value} store : {key:value}
-        // key-> element of array &&& value-> idx of element
+        // logic: 
+        // step 1: declare a hashmap
+        // step 2: traverse the array
+        // step 3: for each current element find complement=target-current
+        // step 4: now check if complement exists in map
+        // if yes then store current element and that map element
 
-        // traversing the array
-        for ( int i=0 ; i<n ; i++ ){
-            // for each element-> check complement already exists in map?
-            int complement = target-nums[i];
-            
-            if ( mpp.find(complement) != mpp.end() ){
-                // yes complement already exists
-                // immediatrly return the complement's idx and current idx
-                return {mpp[complement], i};
+        // to store the result
+        vector<int>result;
+        int complement;
+
+        unordered_map<int,int>mpp; // key - value pair
+        // key : array element
+        // value : index
+        for ( int i=0 ; i<nums.size() ; i++ ){
+            complement = target - nums[i];
+            if (mpp.count(complement)){
+                // if complement exists:
+                // store complement and current element in map.
+                result.push_back(mpp[complement]);
+                result.push_back(i);
+            } else{
+                // if complement do not exists
+                // then directly store that element in map
+                mpp[nums[i]]=i;
             }
-
-            // complement -> do not exists
-            // store current num into mpp
-            mpp[nums[i]]=i;
         }
-    return {};
+
+        return result;
     }
 };
