6d14336fa185d7540e7b3178901cbcc284358f37
Devanshu Jangid <156595156+Devanshujangid@users.noreply.github.com>
2026-07-21 00:11:57 +0530

    Time: 0 ms (100%), Space: 28.2 MB (35.96%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,25 +1,30 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        int n=nums.size();
-        int maj_ele;
-        int count=0;
-         for ( int i=0 ; i<n ; i++ ){
-            // algo
-            if ( count == 0 ){
-                // make it to 1
-                count=1;
-                // update majority element
-                maj_ele=nums[i];
+        // brute force : use hash array -> but it will make S.C=O(n)
+        // optimal : in O(1) space 
+        // boyers moore voting algorithm:
+
+        // we need two varaiables:
+        int candidate=nums[0]; // initilize it with first array element
+        int supporters=0;
+        // now traverse the array- start from index 1
+        for ( int i=0 ; i<nums.size() ; i++ ){
+            // case 1: current element matches the current candidate
+            if ( nums[i]==candidate ){
+                // increment supporter count by 1
+                supporters++;
+            } else{
+                // case 2: decrease the supporter count
+                supporters--;
             }
-            else if( nums[i]==maj_ele ){
-                count++;
-            }
-            else{
-                count--;
+            // if supporter count == 0 
+            if ( supporters==0 ){
+                // then change the candidate
+                candidate = nums[i];
+                supporters++;
             }
         }
-        // majority element always exists->do not need to check anything!
-        return maj_ele;  
+        return candidate;
     }
 };
