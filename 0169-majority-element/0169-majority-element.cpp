844525a94000e2f7f1488f763fe918552f904cba
Devanshu Jangid <156595156+Devanshujangid@users.noreply.github.com>
2026-08-28 22:38:49 +0530

    Time: 0 ms (100%), Space: 28.1 MB (95.17%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,30 +1,69 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        // brute force : use hash array -> but it will make S.C=O(n)
-        // optimal : in O(1) space 
-        // boyers moore voting algorithm:
+        // // brute force : use hash array -> but it will make S.C=O(n)
+        // // optimal : in O(1) space 
+        // // boyers moore voting algorithm:
 
-        // we need two varaiables:
-        int candidate=nums[0]; // initilize it with first array element
-        int supporters=0;
-        // now traverse the array- start from index 1
+        // // we need two varaiables:
+        // int candidate=nums[0]; // initilize it with first array element
+        // int supporters=0;
+        // // now traverse the array- start from index 1
+        // for ( int i=0 ; i<nums.size() ; i++ ){
+        //     // case 1: current element matches the current candidate
+        //     if ( nums[i]==candidate ){
+        //         // increment supporter count by 1
+        //         supporters++;
+        //     } else{
+        //         // case 2: decrease the supporter count
+        //         supporters--;
+        //     }
+        //     // if supporter count == 0 
+        //     if ( supporters==0 ){
+        //         // then change the candidate
+        //         candidate = nums[i];
+        //         supporters=1;
+        //     }
+        // }
+        // return candidate;
+
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        int candidate = nums[0];
+        int supporters = 0;
+
         for ( int i=0 ; i<nums.size() ; i++ ){
-            // case 1: current element matches the current candidate
-            if ( nums[i]==candidate ){
-                // increment supporter count by 1
+            if ( nums[i] == candidate ){
                 supporters++;
-            } else{
-                // case 2: decrease the supporter count
+            } else {
                 supporters--;
             }
-            // if supporter count == 0 
-            if ( supporters==0 ){
-                // then change the candidate
+
+            if ( supporters == 0 ){
                 candidate = nums[i];
-                supporters=1;
+                supporters = 1;
             }
         }
+        
         return candidate;
     }
 };
